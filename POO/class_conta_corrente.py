2e48023d2857ee043323b7423b285e45afa56ee0
Senac Tatuape <senac@localhost.localdomain>
2024-01-23 09:38:08 -0300

    Adição do método sacar da conta corrente

--- POO/class_conta_corrente.py
+++ POO/class_conta_corrente.py
@@ -12,3 +12,18 @@ class ContaCorrente(ContaBancaria):
         self._saldo = saldo
         self.__limite = limite
         print("A conta "+self._nconta+" do Sr(a). "+self._titular+" foi aberta")
+
+    def sacar(self, valor):
+        
+        if(valor > self._saldo+self.__limite):
+            print("Saldo Insuficiente!")
+        
+        elif(valor <= self._saldo):
+            self._saldo -= valor
+            print("Você realizou um saque de "+str(valor)+" o seu saldo atual é "+str(self._saldo))
+
+        else:
+            sobra = valor - self._saldo
+            self.__limite -= sobra
+            self._saldo = 0
+            print("O valor sacado é "+str(valor)+" \no saldo atual é: "+str(self._saldo)+" \no seu limite atual é: "+str(self.__limite))
--- POO/obj_conta_corrente.py
+++ POO/obj_conta_corrente.py
@@ -2,3 +2,4 @@ from class_conta_corrente import ContaCorrente as cc
 
 conta = cc()
 conta.abrirConta(35,12,"123456-10","Marcia",3500,1000)
+conta.sacar(200)
